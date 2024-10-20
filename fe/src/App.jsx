1ac9b25e88245a8729d21b55f53a1d877fa1ac70
Haotq <haotqse173667@gmail.com>
2024-10-21 00:17:57 +0700

    Initial commit

---
+++ fe/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import NavBar from './components/NavBar'
+import {BrowserRouter,Route,Switch} from 'react-router-dom'
+import Cart from './components/Cart'
+import Home from './components/Home'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    
+      <div className='app'>
+        
+      </div>
+     
+    
+  )
+}
+
+export default App
